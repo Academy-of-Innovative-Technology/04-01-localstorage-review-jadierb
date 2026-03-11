113a9ff4824ff45d873184c93f3fee533e1c605c
jadier <144461536+jadierb@users.noreply.github.com>
2026-03-11 00:11:12 +0000

    got the key from the input

--- script.js
+++ script.js
@@ -9,15 +9,25 @@ document.querySelector(".save").onclick = function() {
 
     if (saveKey && saveValue) {
         localStorage.setItem(saveKey, saveValue);
-        
+
+        document.querySelector("#saveKey").value = "";
+        document.querySelector("#saveValue").value = "";
+        alert("Data saved successfully;");
+    } else {
+        alert("please enter both key and value fields.");
     }
+
+    };
+    
 
     
 
 
 // LOAD
 document.querySelector(".load").onclick = function() {
+const loadKey = document.querySelector("#loadkey").value;
 
+if (loadKey) {}
 };
 
 // LOAD
